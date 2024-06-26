06dfe6e48069c83c7f94d5068b49a4eec4b420fd
Rishabh Harod <rishabhharod4449@gmail.com>
2024-06-26 12:09:30 +0530

    learn

---
+++ first.js
@@ -0,0 +1,66 @@
+// const sy=Symbol["key1"]
+// console.log(JsUser);
+// Object.freeze(JsUser)
+// // JsUser.email="rishabh@bestpeers.com";
+// console.log(JsUser["email"])
+// const qw1={1:"a",2: "b"}
+// const qw2={2:"c",3:"d"}
+// const qw3=Object.assign({},qw1,qw2);
+// console.log(qw3);
+// console.log(qw1);
+// const array1=[
+//     {
+//         email:"rishabh@gmail.com",
+//         id:101
+//     }
+//     ,
+//     {
+//         email:"sheetal@gmail.com",
+//         id:102
+//     }
+//     ,
+//     {
+//         email:"samruddhi@gmail.com",
+//         id:103
+//     }
+// ]
+//     const JsUser ={}
+//     JsUser.id=123
+//     JsUser.name="rishabh"
+//     JsUser.email="rishabh@gmail.com";
+// // console.log(array1[2].email)
+// const{name:n}=JsUser;
+// console.log(n)
+// add(5,6);
+// const add = function(num1,num2)
+// {
+//     return num1+num2;
+// }
+
+console.log(sum(5,6));
+
+function sum(num1,num2){
+    return num1+num2;
+}
+// function arrayOfAdd(arr)
+// {
+//     let sum=0;
+   
+//     for(let i=0;i<arr.length;i++)
+//         {
+//             sum=sum+arr[i]
+//         }
+//         return sum;
+// }
+// let arr1=[1,2,4,5,6,5,6,5,4,7]
+// let result=arrayOfAdd(arr1)
+// console.log(result);
+
+
+
+
+
+
+
+
+
